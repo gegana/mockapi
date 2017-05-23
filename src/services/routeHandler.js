ff1dd0b384034e21264c8e4e6c7202d7f6dde4b6
Dylan Sumiskum <dsumiskum@vistaprint.com>
2017-05-23 11:28:44 -0400

    feature: added conditional behavior core components.

--- src/services/routeHandler.js
+++ src/services/routeHandler.js
@@ -1,5 +1,7 @@
 const util = require('util')
 
+var counter = {}
+
 module.exports = class RouteHandler {
     constructor(route) {
         this.__route = route
@@ -16,9 +18,31 @@ module.exports = class RouteHandler {
         try {
             var params = params || req.params
             var query = query || req.query
-            var body = this.__processBodyTokens(params, query)
+            var tokens = {}
+
+            merge(tokens, params)
+            merge(tokens, query)
+
+            if (!(req.originalUrl in counter)) {
+                counter[req.originalUrl] = 0
+            }
+            counter[req.originalUrl] += 1
+
+            tokens.calls = counter[req.originalUrl]
+
+            var status = this.__route.status,
+                body = this.__processBodyTokens(this.__route.body, tokens)
+
+            if (this.__route.conditionalBehaviors && Array.isArray(this.__route.conditionalBehaviors)) {
+                var behavior = this.__processConditionalBehaviors(tokens, status, body)
+                if (behavior) {
+                    status = behavior.status
+                    body = behavior.body
+                }
+            }
+
             await this.__processResponseDelay()
-            res.status(this.__route.status).send(body)
+            res.status(status).send(body)
         } catch (err) {
             next(err)
         }
@@ -26,22 +50,15 @@ module.exports = class RouteHandler {
 
     /**
      * Populate tokens in route body template with values from params and query
-     * @param {object} params - Request url parameters keyed by parameter name
-     * @param {object} query - Request querystring parameters keyed by parameter name
+     * @param {object} body - Route body template
+     * @param {object} tokens - Request url parameters, querystring parameters, and other info related to the request
      */
-    __processBodyTokens(params, query) {
-        if (!this.__route.body) {
+    __processBodyTokens(body, tokens) {
+        if (!body) {
             return {}
         }
-        var s = JSON.stringify(this.__route.body)
-        for (let k in params) {
-            var regex = new RegExp('\{(' + k + ')\}')
-            s = s.replace(regex, params[k])
-        }
-        for (let k in query) {
-            var regex = new RegExp('\{(' + k + ')\}')
-            s = s.replace(regex, query[k])
-        }
+        var s = JSON.stringify(body)
+        s = parseTokens(s, tokens)
         return JSON.parse(s)
     }
 
@@ -51,6 +68,24 @@ module.exports = class RouteHandler {
         }
         return false
     }
+
+    /**
+     * Process conditional behaviors by their priority order and returning the first behavior where the condition evaluates to true
+     * @param {object} tokens - Request url parameters, querystring parameters, and other info related to the request
+     */
+    __processConditionalBehaviors(tokens) {
+        for (var i in this.__route.conditionalBehaviors) {
+            var behavior = this.__route.conditionalBehaviors[i]
+            var condition = behavior.condition
+            condition = parseTokens(condition, tokens)
+            if (eval(condition) == true) {
+                return {
+                    status: behavior.status,
+                    body: this.__processBodyTokens(behavior.body, tokens)
+                }
+            }
+        }
+    }
 }
 
 /**
@@ -59,4 +94,28 @@ module.exports = class RouteHandler {
  */
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Merge properties from src to obj
+ * @param {object} obj 
+ * @param {object} src 
+ */
+function merge(obj, src) {
+    for (let k in src) {
+        obj[k] = src[k]
+    }
+}
+
+/**
+ * Parse the tokens in a string
+ * @param {string} s 
+ * @param {array} tokens 
+ */
+function parseTokens(s, tokens) {
+    for (let k in tokens) {
+        var regex = new RegExp('\{(' + k + ')\}')
+        s = s.replace(regex, tokens[k])
+    }
+    return s
 }
